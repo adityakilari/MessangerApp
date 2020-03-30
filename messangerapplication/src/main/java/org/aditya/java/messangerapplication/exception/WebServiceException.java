4f2c3b52d1d354b0c0d05fc25b0f8277f7c589b4
adityakilari <sriaditya.k@gmail.com>
2020-03-30 16:37:36 +0530

    small change

--- messangerapplication/src/main/java/org/aditya/java/messangerapplication/exception/WebServiceException.java
+++ messangerapplication/src/main/java/org/aditya/java/messangerapplication/exception/WebServiceException.java
@@ -8,6 +8,8 @@ public class WebServiceException extends RuntimeException{
 	private static final long serialVersionUID = 2691825590413871312L;
 	
 	 public WebServiceException(String message) {
+		 
+		 
 		 super(message);
 	 }
 	
